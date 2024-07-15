9e99fc4ea4a40d81aa688bd36267210d94927578
Girjesh786 <95648184+Girjesh786@users.noreply.github.com>
2024-07-15 16:23:31 +0530

    Assignment20.py

--- Assignment20.py
+++ Assignment20.py
@@ -1,9 +1,15 @@
-# Input dictionary
+#Write a Python program to get the key, value and item in a dictionary.
+input:dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
-
-# Printing the header
 print("key  value")
 
-# Printing keys and values
 for key, value in dict_num.items():
     print(f"{key}    {value}")
+"""Output: key  value
+1    10
+2    20
+3    30
+4    40
+5    50
+6    60
+"""
